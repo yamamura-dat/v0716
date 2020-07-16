855edbd101177451750ae24074243d6d0b7f3009
satthubongdem <49702341+satthubongdem@users.noreply.github.com>
2020-07-16 10:25:22 +0900

    完成

--- v0716/Form1.cs
+++ v0716/Form1.cs
@@ -16,5 +16,64 @@ namespace v0716
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "-1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "-1";
+            textBox2.Text = "0";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "1";
+            textBox2.Text = "0";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "1";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "-1";
+            textBox2.Text = "-1";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "1";
+            textBox2.Text = "-1";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "-1";
+            textBox2.Text = "1";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "1";
+            textBox2.Text = "1";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "(´･ω･`)";
+        }
     }
 }
